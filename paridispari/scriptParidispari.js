89a9b34a1a360d0c13a54d7b9a7d3b59f8126ec5
Boato Andrea <163646394+aandrea-boatoo@users.noreply.github.com>
2024-11-05 16:38:52 +0100

    pari e dispari exercise

---
+++ paridispari/scriptParidispari.js
@@ -0,0 +1,46 @@
+"use strict";
+console.clean;
+
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+const scommessa = prompt("scegli fra pari o dispari");
+const numUser = parseInt(prompt("scegli un numero da 1 a 5"));
+const numPc = random(1, 5);
+
+pariDispari(scommessa, numUser, numPc)
+
+/**
+ * 
+ * @param {*} parDis 
+ * @param {*} user 
+ * @param {*} pc 
+ */
+
+function pariDispari(parDis, user, pc){
+    let somma = user + pc;
+    if((somma % 2 == 0 && parDis === "pari") || (somma % 2 === 1 && parDis === "dispari")){
+        console.log(`Congratulazioni hai vinto, infatti ${user} + ${pc} (numero del pc) fa ${somma}`)
+    }else{
+        console.log(`Peccato hai perso, sara' per la prossima, infatti ${user} + ${pc} (numero del pc) fa ${somma}`)
+
+    }
+}
+
+
+/**
+ * 
+ * @param {*} min 
+ * @param {*} max 
+ * @returns {number}
+ */
+
+function random(min, max){
+    const numeroRandom =  Math.floor(Math.random() * (max - min + 1 )) + min ;
+     // console.log(numeroRandom)
+     return numeroRandom;
+ }
+
