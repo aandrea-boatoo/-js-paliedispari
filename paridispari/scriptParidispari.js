0be187aff0bff69978da34357b99efd4b6ac4112
Boato Andrea <163646394+aandrea-boatoo@users.noreply.github.com>
2024-11-05 17:07:59 +0100

    verified input

--- paridispari/scriptParidispari.js
+++ paridispari/scriptParidispari.js
@@ -10,37 +10,31 @@ console.clean;
 const scommessa = prompt("scegli fra pari o dispari");
 const numUser = parseInt(prompt("scegli un numero da 1 a 5"));
 const numPc = random(1, 5);
-
 pariDispari(scommessa, numUser, numPc)
 
-/**
- * 
- * @param {*} parDis 
- * @param {*} user 
- * @param {*} pc 
- */
-
-function pariDispari(parDis, user, pc){
-    let somma = user + pc;
-    if((somma % 2 == 0 && parDis === "pari") || (somma % 2 === 1 && parDis === "dispari")){
-        console.log(`Congratulazioni hai vinto, infatti ${user} + ${pc} (numero del pc) fa ${somma}`)
-    }else{
-        console.log(`Peccato hai perso, sara' per la prossima, infatti ${user} + ${pc} (numero del pc) fa ${somma}`)
-
-    }
-}
 
 
-/**
- * 
- * @param {*} min 
- * @param {*} max 
- * @returns {number}
- */
 
 function random(min, max){
     const numeroRandom =  Math.floor(Math.random() * (max - min + 1 )) + min ;
-     // console.log(numeroRandom)
-     return numeroRandom;
- }
+    return numeroRandom;
+}
 
+
+function pariDispari(parDis, user, pc){
+    let somma = user + pc;
+    if((parDis === "pari" ) || (parDis === "dispari") ){
+        if (!(user <= 5) && (user >= 1)){
+            console.log(`input non valido, scegli un numero tra 1 e 5, purtroppo "${user}" non rientra nei parametri`);    
+        } else {
+
+            if((somma % 2 == 0 && parDis === "pari") || (somma % 2 === 1 && parDis === "dispari")){
+                console.log(`Congratulazioni hai vinto, infatti ${user} + ${pc} (numero del pc) fa ${somma}`)
+            }else{
+                console.log(`Peccato hai perso, sara' per la prossima, infatti ${user} + ${pc} (numero del pc) fa ${somma}`)
+            }
+        }   
+    }else{
+        console.log(`hai inserito un input non valido, ricorda che puoi scegliere solo "pari" e "dispari",  mentre tu hai digitato "${parDis}"`)
+    }
+}
